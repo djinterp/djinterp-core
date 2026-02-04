00ab2caa5ec309f75232649efd420250e3beacd3
Sam Neal-Blim <teer@nealblim.com>
2026-02-04 09:35:17 -0500

    improved commenting, formatting

--- inc/string_fn.h
+++ inc/string_fn.h
@@ -8,54 +8,102 @@
 * link:      TBA
 * author(s): Samuel 'teer' Neal-Blim                          date: 2025.10.26
 ******************************************************************************/
+/*
+TABLE OF CONTENTS
+=================
+i.    Safe string copy & string concatenation
+      ---------------------------------------
+      a.  d_strcpy_s
+      b.  d_strncpy_s
+      c.  d_strcat_s
+      d.  d_strncat_s
+      
+ii.   String duplication
+      ------------------
+      a.  d_strdup
+      b.  d_strndup
+      
+iii.  Case-insensitive comparison
+      ---------------------------
+      a.  d_strcasecmp
+      b.  d_strncasecmp
+      
+iv.   Thread-safe tokenization
+      ------------------------
+      a.  d_strtok_r
+      
+v.    String length with limit
+      ------------------------
+      a.  d_strnlen
+      
+vi.   Case-insensitive subtring search
+      ---------------------------------
+      a.  d_strcasestr
+      
+vii.  String case conversion
+      ----------------------
+      a.  d_strlwr
+      b.  d_strupr
+
+viii. String reversal
+      ---------------
+      a.  d_strrev
+
+ix.   Character search that returns end pointer
+      -----------------------------------------
+      a.  d_strchrnul
+      
+x.    Thread-safe error string
+      ------------------------
+      a.  d_strerror_r
+*/
 
 #ifndef DJINTERP_STRING_FN_
 #define DJINTERP_STRING_FN_ 1
 
-#include <ctype.h>
-#include <stddef.h> 		// for size_t
+#include <stddef.h>         // for size_t
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include ".\djinterp.h"		// 
-#include ".\dmemory.h"		// 
+#include ".\djinterp.h"
+#include ".\dmemory.h"
 
 
-// safe string copy (C11 `strcpy_s` equivalent)
-int    d_strcpy_s(char* restrict _destination, size_t _dest_size, const char* restrict _src);
-int    d_strncpy_s(char* restrict _destination, size_t _dest_size, const char* restrict _src, size_t _count);
-int    d_strcat_s(char* restrict _destination, size_t _dest_size, const char* restrict _src);
-int    d_strncat_s(char* restrict _destination, size_t _dest_size, const char* restrict _src, size_t _count);
+// i.   Safe string copying & concatenation
+int      d_strcpy_s(char* restrict _destination, size_t _dest_size, const char* restrict _src);
+int      d_strncpy_s(char* restrict _destination, size_t _dest_size, const char* restrict _src, size_t _count);
+int      d_strcat_s(char* restrict _destination, size_t _dest_size, const char* restrict _src);
+int      d_strncat_s(char* restrict _destination, size_t _dest_size, const char* restrict _src, size_t _count);
 
-// string duplication (POSIX `strdup` equivalent)
-char*  d_strdup(const char* _str);
-char*  d_strndup(const char* _str, size_t _n);
+// ii.   String duplication
+char*    d_strdup(const char* _str);
+char*    d_strndup(const char* _str, size_t _n);
 
-// case-insensitive comparison (POSIX `strcasecmp` equivalent)
-int    d_strcasecmp(const char* _str2, const char* _s2);
-int    d_strncasecmp(const char* _str2, const char* _s2, size_t _n);
+// iii.  Case-insensitive comparison
+int      d_strcasecmp(const char* _str2, const char* _s2);
+int      d_strncasecmp(const char* _str2, const char* _s2, size_t _n);
 
-// thread-safe tokenization (POSIX `strtok_r` equivalent)
-char*  d_strtok_r(char* restrict _str, const char* restrict _delim, char** restrict _saveptr);
+// iv.   Thread-safe tokenization
+char*    d_strtok_r(char* restrict _str, const char* restrict _delim, char** restrict _saveptr);
 
-// string length with limit (C11 `strnlen` equivalent)
-size_t d_strnlen(const char* _str, size_t _maxlen);
+// v.    String length with limit
+size_t   d_strnlen(const char* _str, size_t _maxlen);
 
-// case-insensitive substring search (GNU `strcasestr` equivalent)
-char*  d_strcasestr(const char* _haystack, const char* _needle);
+// vi.   Case-insensitive substring search
+char*    d_strcasestr(const char* _haystack, const char* _needle);
 
-// string case conversion (Windows `_strlwr`/`_strupr` equivalent)
-char*  d_strlwr(char* _str);
-char*  d_strupr(char* _str);
+// vii.  String case conversion
+char*    d_strlwr(char* _str);
+char*    d_strupr(char* _str);
 
-// string reversal (Windows `_strrev` equivalent)
-char*  d_strrev(char* _str);
+// viii. String reversal
+char*    d_strrev(char* _str);
 
-// character search that returns end pointer (GNU `strchrnul` equivalent)
-char*  d_strchrnul(const char* _str, int _c);
+// ix.   Character search that returns end pointer
+char*    d_strchrnul(const char* _str, int _c);
 
-// thread-safe error string (POSIX `strerror_r` equivalent)
-int    d_strerror_r(int _errnum, char* _buf, size_t _buflen);
+// x.    Thread-safe error string
+int      d_strerror_r(int _errnum, char* _buf, size_t _buflen);
 
 
-#endif	// DJINTERP_STRING_FN_
+#endif    // DJINTERP_STRING_FN_
