57284566f60527b529ae06adbf4c0582e37d211b
Sam 'teer' Neal-Blim <dev@nealblim.com>
2026-02-15 17:04:06 -0500

    Added 'dconfig.h': framework build configuration.

---
+++ inc/dconfig.h
@@ -0,0 +1,17 @@
+/******************************************************************************
+* djinterp [core]                                                    dconfig.h
+*
+* Configuration defines.
+* 
+* 
+* path:      \inc\dconfig.h
+* link(s):   TBA
+* author(s): Samuel 'teer' Neal-Blim                          date: 2025.10.15
+******************************************************************************/
+
+#ifndef DJINTERP_CONFIG_
+#define DJINTERP_CONFIG_ 1
+
+
+
+#endif // DJINTERP_CONFIG_
